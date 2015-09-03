528102fd6f6a5c12bb395ef6965543bb1af43e72
Andre Luiz A Viana <and.luizalmeida@gmail.com>
2015-09-03 11:58:02 -0300

    START

---
+++ BasicMVC/Controllers/XyzController.cs
@@ -0,0 +1,43 @@
+﻿using BasicMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicMVC.Controllers
+{
+    public class XyzController : Controller
+    {
+        // GET: Xyz
+        public JsonResult Abc()
+        {
+            Student andre = new Student();
+            andre.studentId = 1;
+            andre.firstName = "André Luiz";
+            andre.lastName = "Almeida Viana";
+
+            Student lynda = new Student();
+            lynda.studentId = 2;
+            lynda.firstName = "Lynda";
+            lynda.lastName = "Berry";
+
+            Student john = new Student();
+            john.studentId = 3;
+            john.firstName = "John";
+            john.lastName = "Doe";
+
+            List<Student> students = new List<Student>();
+            students.Add(andre);
+            students.Add(lynda);
+            students.Add(john);
+
+            return Json(students, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BasicMVC/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicMVC.Models
+{
+    public class Student
+    {
+        public int studentId { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+    }
+}
